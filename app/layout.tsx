7bd061a1dc6bc652a71b6b670dcbd260f86df09d
crowddevelopers08-arch <crowddevelopers08@gmail.com>
2026-05-23 11:47:01 +0530

    hair o graft code attached completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Outfit } from "next/font/google";
 import Script from "next/script";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,8 +26,18 @@ export const metadata: Metadata = {
       { url: "/image.png", sizes: "180x180", type: "image/png" },
     ],
     other: [
-      { rel: "icon", url: "/image.png", sizes: "192x192", type: "image/png" },
-      { rel: "icon", url: "/image.png", sizes: "512x512", type: "image/png" },
+      {
+        rel: "icon",
+        url: "/image.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        rel: "icon",
+        url: "/image.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
     ],
   },
 };
@@ -45,20 +54,67 @@ export default function RootLayout({
     >
       <head>
         {/* Google Tag Manager */}
-        <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-        >
+        <Script id="gtm-script" strategy="afterInteractive">
           {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];
-            w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});
-            var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-            j.async=true;
-            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-            f.parentNode.insertBefore(j,f);
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({
+                'gtm.start': new Date().getTime(),
+                event:'gtm.js'
+              });
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),
+              dl=l!='dataLayer'?'&l='+l:'';
+
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-KT6P6DWN');
+          `}
+        </Script>
+
+        {/* Google Ads Tag */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-18129704043"
+          strategy="afterInteractive"
+        />
+
+        {/* Google Ads Phone Conversion + Click Call Conversion */}
+        <Script id="google-ads" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+
+            function gtag(){
+              dataLayer.push(arguments);
+            }
+
+            gtag('js', new Date());
+
+            // Base Google Ads Tag
+            gtag('config', 'AW-18129704043');
+
+            // Phone Conversion
+            gtag('config', 'AW-18129704043/RSLBCK7C07EcEOuo9cRD', {
+              phone_conversion_number: '7448866675'
+            });
+
+            // Click-to-call conversion
+            window.gtag_report_conversion = function(url) {
+              var callback = function () {
+                if (typeof(url) != 'undefined') {
+                  window.location = url;
+                }
+              };
+
+              gtag('event', 'conversion', {
+                send_to: 'AW-18129704043/NiRdCLHC07EcEOuo9cRD',
+                value: 1.0,
+                currency: 'INR',
+                event_callback: callback
+              });
+
+              return false;
+            };
           `}
         </Script>
       </head>
@@ -74,7 +130,9 @@ export default function RootLayout({
           />
         </noscript>
 
-        <main className="flex-1">{children}</main>
+        <main className="flex-1">
+          {children}
+        </main>
       </body>
     </html>
   );
